455b359445287e2d02c351498451e317e70cac91
Johan Mattsson <johan.mattsson@hotmail.se>
2020-01-13 16:07:09 +0100

    Add attribute Scanner to Library
    Will be used to read input from user

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -2,8 +2,10 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Library {
     private List<Book> books = new ArrayList<>();
     private List<User> users = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
 }
