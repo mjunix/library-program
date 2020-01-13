b0cdb723ffdb78371092a0a9fb470de30990921a
Johan Mattsson <johan.mattsson@hotmail.se>
2020-01-13 18:29:23 +0100

    Add constructors to Book, Librarian and User

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -4,4 +4,10 @@ public class Book {
     private String title;
     private String author;
     private String description;
+
+    public Book(String title, String author, String description) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+    }
 }
