ec9f546c0eb455a1fa63065340b08f6b488d769d
Johan Mattsson <johan.mattsson@hotmail.se>
2020-01-15 23:50:35 +0100

    Getter for name in User

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -12,6 +12,10 @@ public class User implements Serializable {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
         return this.getClass().getSimpleName() + "{" +
