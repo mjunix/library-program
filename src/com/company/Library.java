4571bdfc5d4dbfc23c8db5c08e420ec709a54227
Johan Mattsson <johan.mattsson@hotmail.se>
2020-01-18 15:31:35 +0100

    Fix loop not terminating correctly

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -201,6 +201,7 @@ public class Library {
 
             Book bookToBorrow = availableBooks.remove(index);
             loggedInUser.borrowBook(bookToBorrow);
+            break;
         }
     }
 
